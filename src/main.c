b331b4a52ce3a3ef01222bcdb73cc392478cbe30
Giovanni Forma <bluehairguyiscool@gmail.com>
2023-12-30 17:49:23 -0800

    dsf

--- src/main.c
+++ src/main.c
@@ -1,11 +1,6 @@
-#include <stdint.h>
 #include <stdio.h>
-
-uint16_t rgb(uint8_t r, uint8_t g, uint8_t b) {
-    return ((uint16_t)(b >> 3) << 11) | ((uint16_t)(g >> 2) << 5) | (r >> 3);
-}
+#include <stdlib.h>
 
 int main() {
-    printf("%d", rgb(255, 255, 255));
     return 0;
 }
